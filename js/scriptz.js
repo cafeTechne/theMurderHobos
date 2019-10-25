688cfee94ff63077e8cca4c9b01eda9ff1aca51a
Gino <cafeTechne@gmail.com>
2019-10-24 23:30:12 -0400

    *Far too many changes to record in a commit message--mainly focused on a mobile first design. Reworked the Tour page to use cards. Tons of CSS work.

---
+++ js/scriptz.js
@@ -0,0 +1,52 @@
+
+        var character1 = window.document.getElementById("character1");
+        var character2 = window.document.getElementById("character2");
+        var character3 = window.document.getElementById("character3");            var character4 = window.document.getElementById("character4");
+        var character5 = window.document.getElementById("character5");
+        var character6 = window.document.getElementById("character6");
+        var character7 = window.document.getElementById("character7");
+        var instructions = document.getElementById("instructionsForAboutPage");
+        
+        
+
+
+        function hideAllSections(){
+
+            character1.style.display = "none";
+            character2.style.display = "none";
+            character3.style.display = "none";
+            character4.style.display = "none";
+            character5.style.display = "none";
+            character6.style.display = "none";
+            character7.style.display = "none";
+            instructions.style.display = "none";
+        }
+
+
+        function getCharacterInformation(characterName){
+            if(characterName == "Pat") {hideAllSections(); character1.style.display = "block"; }
+            if(characterName == "Dan") {hideAllSections(); character2.style.display = "block"; }
+            if(characterName == "Lindsey") {hideAllSections(); character3.style.display = "block"; }
+            if(characterName == "JD") {hideAllSections(); character4.style.display = "block"; }
+            if(characterName == "Joe") {hideAllSections(); character5.style.display = "block"; }
+            if(characterName == "Garth") {hideAllSections(); character6.style.display = "block"; }
+            if(characterName == "Urg") {hideAllSections(); character7.style.display = "block"; }
+        }
+        
+
+        setTimeout(function clickOnStart(){document.getElementById("cn-button").click();}, 200);
+            
+        function hideTheH1Node(){
+            var h1Node = window.document.getElementsByTagName("h1");
+            h1Node[0].style.display = "none";
+        }
+
+        function showTheH1Node(){
+            var h1Node = window.document.getElementsByTagName("h1");
+            h1Node[0].style.display = "block";
+        }
+
+        function hideTheHeader(){
+            var headerNode = window.document.getElementsByTagName("nav");
+            headerNode[0].style.display = "none";            
+        }
